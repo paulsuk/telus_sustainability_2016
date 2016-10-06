2a054df0aee8d8ae14eae16838549b0527c40f2c
paulsuk <paul.suk@mail.utoronto.ca>
2016-10-06 01:32:29 -0400

    more creative

--- receiveText.py
+++ receiveText.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import random
 
 app = Flask(__name__)
+possible_response = ["What did you say to me little bitch?", "Get Out", "You're dead to me", "Bye Felicia", "Just fuck me up fam"]
 
 @app.route("/", methods=['GET', 'POST'])
 def respond_to_call():
 	resp = twilio.twiml.Response()
-	resp.message("What did you say to me little bitch?")
+	randomNum = random.random()
+	index = int(randomNum*5)
+
+	resp.message(possible_response[index])
 	return str(resp)
 
 if __name__ == "__main__":
