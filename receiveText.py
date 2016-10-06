62ad58530096a7d39c30101d6302fce95e63392c
paulsuk <paul.suk@mail.utoronto.ca>
2016-10-06 01:04:06 -0400

    hello

--- receiveText.py
+++ receiveText.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def respond_to_call():
-	print("got something")
 	resp = twilio.twiml.Response()
 	resp.message("What did you say to me little bitch?")
 	return str(resp)
